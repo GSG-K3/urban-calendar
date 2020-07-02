82e8dec199da609e24038dcf9c5a383cbbb47fa1
Nujood <shawar.nujood@gmail.com>
2020-07-02 10:33:54 +0300

    make the connection file #30

---
+++ server/database/dbConnection.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg");
+require("env2")('./config.env')
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) throw new Error("Failed to connect to DataBase");
+
+module.exports = new Pool({
+  connectionString,
+  ssl: !connectionString.includes("localhost"),
+});
