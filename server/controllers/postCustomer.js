c54d059478ef46a4dfd9fc397c795f2cd89e8835
tasneembhiri <141059@ppu.edu.ps>
2020-07-05 10:11:39 +0300

    create controller file for post customer information #41

---
+++ server/controllers/postCustomer.js
@@ -0,0 +1,21 @@
+const postCustomer = require('../database/queries/postCustomer');
+
+module.exports = (req, res) => {
+  const data = req.body;
+  const { firstName, lastName, phone, email } = data;
+
+  if (!firstName || !lastName || !phone || !email) {
+    return res.status(400).json({
+      message: 'There is one item or more missing. Please filled it!',
+      status: 400,
+    });
+  }
+  postCustomer(data)
+    .then((response) => {
+      if (response.rowCount === 1) {
+        return res.status(200).json({ message: 'user created successfully!' });
+      }
+    })
+
+    .catch((err) => res.status(500).json({ message: err.message }));
+};
