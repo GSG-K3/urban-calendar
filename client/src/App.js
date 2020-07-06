9c284e364cd56c68ebc6616bca9aa5aac68b0e21
Nujood <shawar.nujood@gmail.com>
2020-07-06 10:43:27 +0300

    initiating react router-dom for landing page #46

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/layouts/LandingPage';
+
+const App = () => (
+  <Fragment>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  </Fragment>
+);
 
 export default App;
