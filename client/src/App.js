5238732b3a5eb48a327de68c0f024000201f9644
Nujood <shawar.nujood@gmail.com>
2020-07-06 18:28:34 +0300

    inserting questions route into the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/layouts/LandingPage';
-
+import Checkout from './components/commonComponents/Tabs'
 const App = () => (
   <Fragment>
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path= "/questions" component={Checkout}/>
       </Switch>
     </Router>
   </Fragment>
