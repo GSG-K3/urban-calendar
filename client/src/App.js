96c2af965b891890b98ce167ecc28ef0be435579
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-06 15:25:01 +0200

    create router to the guideline page #23

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/layouts/LandingPage';
+import Guideline from './components/layouts/Guideline';
 
 const App = () => (
   <Fragment>
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/guideline" component={Guideline} />
       </Switch>
     </Router>
   </Fragment>
