eb030ac38b3c6410f299078100beb820823444e6
tasneembhiri <141059@ppu.edu.ps>
2020-07-07 14:45:41 +0300

    add grid material ui to list and header and line #48

--- client/src/components/commonComponents/navBar/index.js
+++ client/src/components/commonComponents/navBar/index.js
@@ -1,31 +1,59 @@
 import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import logo from './logo.svg';
-// import header from './Header.svg';
 import useStyles from './style.js';
 
 const NavBar = () => {
   const classes = useStyles();
   return (
     <div>
-      {/* <img src={header} /> */}
-      <div className={classes.header}>
-        <p>Our Showroom is Open! (By Appointment Only)</p>
-      </div>
+      <Grid container>
+        <Grid item xs={12} sm={12} className={classes.header}>
+          <p>Our Showroom is Open! (By Appointment Only)</p>
+        </Grid>
+      </Grid>
       <div className={classes.logo}>
         <img src={logo} className={classes.img} />
         urban natural home
       </div>
+
       <List className={classes.list}>
-        <ListItem> LIVING SALE</ListItem>
-        <ListItem> COLLECTIONS</ListItem>
-        <ListItem> FURNITURE</ListItem>
-        <ListItem> ORIGINAL MATTRESSES</ListItem>
-        <ListItem> LIGHTING & ACCENTS </ListItem>
-        <ListItem> GET TO KNOW US</ListItem>
-        <ListItem className={classes.item}> BOOK A VISIT</ListItem>
+        <Grid container>
+          <Grid item sm={3} md={2}>
+            <ListItem> COLLECTIONS</ListItem>
+          </Grid>
+          <Grid item sm={3} md={2}>
+            <ListItem> FURNITURE</ListItem>
+          </Grid>
+          <Grid item sm={3} md={2}>
+            <ListItem> ORIGINAL MATTRESSES</ListItem>
+          </Grid>
+          <Grid item sm={3} md={2}>
+            <ListItem> LIGHTING & ACCENTS </ListItem>
+          </Grid>
+          <Grid item sm={3} md={2}>
+            <ListItem> GET TO KNOW US</ListItem>
+          </Grid>
+          <Grid item sm={3} md={2}>
+            <ListItem className={classes.item}> BOOK A VISIT</ListItem>
+          </Grid>
+        </Grid>
       </List>
+
+      <div>
+        <Typography className={classes.intruduction}>
+          Home / Book an Appointment to Visit Our Showroom
+        </Typography>
+        <Typography className={classes.bookappointment}>
+          Book an Appointment to Visit Our Showroom
+        </Typography>
+      </div>
+      <Grid xs={12}>
+        <hr className={classes.line} />
+      </Grid>
     </div>
   );
 };
