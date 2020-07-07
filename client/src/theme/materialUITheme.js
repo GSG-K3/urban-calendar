19955b55eed5c1f6b5874755e78011d6307f553c
Nujood <shawar.nujood@gmail.com>
2020-07-07 14:44:07 +0300

    updating the material ui theme colors

--- client/src/theme/materialUITheme.js
+++ client/src/theme/materialUITheme.js
@@ -3,16 +3,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#90B27A',
       main: '#90B27A',
-      dark: '#002884',
+      dark: '#5E8554',
       contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
     },
   },
 });
