49f4ed7f2ba1431b81656d8bf95a714fc04109ee
tasneembhiri <141059@ppu.edu.ps>
2020-07-07 14:47:13 +0300

    make navbar responsive #48

--- client/src/components/commonComponents/navBar/style.js
+++ client/src/components/commonComponents/navBar/style.js
@@ -1,10 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    height: '100vh',
-  },
   header: {
     backgroundColor: '#000000',
     color: 'white',
@@ -17,21 +13,47 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop:'15px',
+    marginTop: '20px',
+    fontFamily: 'Quicksand',
+    fontSize: '25px',
   },
   img: {
-    width: '32px',
-    height: '32px',
+    width: '60px',
+    height: '60px',
   },
 
   list: {
-    display: 'flex',
-    flexDirection: 'row',
     fontSize: '12px',
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      marginLeft: '65px',
+    },
   },
   item: {
     color: '#90B27B',
     fontWeight: 'bold',
+  },
+  intruduction: {
+    fontFamily: 'Quicksand',
+    marginLeft: '20px',
+    fontSize: '12px',
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      marginLeft: '80px',
+      fontSize: '15px',
+      marginTop: '20px',
+    },
+  },
+  bookappointment: {
+    fontSize: '18px',
+    fontFamily: 'Quicksand',
+    marginLeft: '20px',
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      fontSize: '28px',
+      marginLeft: '80px',
+    },
+  },
+
+  line: {
+    width: '90vw',
   },
 }));
 
