735811857a4d4a24baf3ddef3a352fd05129e636
Nujood <shawar.nujood@gmail.com>
2020-07-07 14:30:12 +0300

    modifying the reservation tabs route endpoint- front end #18

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './components/layouts/LandingPage';
-import Checkout from './components/commonComponents/Tabs'
+import Checkout from './components/commonComponents/Tabs';
+
 const App = () => (
   <Fragment>
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route exact path= "/questions" component={Checkout}/>
+        <Route exact path="/reservation" component={Checkout} />
       </Switch>
     </Router>
   </Fragment>
