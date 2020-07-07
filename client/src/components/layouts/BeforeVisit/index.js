cfcce3cd9ff267f9c8c914a52bbccc8c21df9a45
Nujood <shawar.nujood@gmail.com>
2020-07-07 18:51:04 +0300

    making before visit page- main features #25

---
+++ client/src/components/layouts/BeforeVisit/index.js
@@ -0,0 +1,156 @@
+import React, { Fragment, useState } from 'react';
+import {
+  Typography,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  Checkbox,
+  FormGroup,
+} from '@material-ui/core';
+import style from './style';
+
+const BeforeVisit = () => {
+  const classes = style();
+  const [meeting, setMeeting] = useState();
+  const [furniture, setFurniture] = useState();
+  const [covid, setCovid] = useState();
+
+  const handleChangeMeeting = (event) => {
+    setMeeting(event.target.meeting);
+  };
+  const handleChangeCovid = (event) => {
+    setCovid(event.target.covid);
+  };
+  const handleChangeFurniture = (event) => {
+    setFurniture(event.target.furniture);
+  };
+
+  return (
+    <Fragment>
+      <Typography varient="h5" gutterBottom>
+        Before visit
+      </Typography>
+      <Typography varient="h6" gutterBottom>
+        Would you like to reserve a virtual appointment or in person?{' '}
+      </Typography>
+      <FormControl component="fieldset">
+        <RadioGroup
+          aria-label="meeting"
+          name="meeting1"
+          value={meeting}
+          onChange={handleChangeMeeting}
+        >
+          <FormControlLabel
+            value="In person"
+            control={<Radio color="primary" />}
+            label="In person"
+          />
+          <FormControlLabel
+            value="virtual "
+            control={<Radio color="primary" />}
+            label="Virtual"
+          />
+        </RadioGroup>
+      </FormControl>
+      <Typography varient="h6" gutterBottom>
+        What are you interested in viewing? Select all that apply.
+      </Typography>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={furniture}
+                onChange={handleChangeFurniture}
+                name="Bedroom"
+              />
+            }
+            label="Bedroom"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={furniture}
+                onChange={handleChangeFurniture}
+                name="Dining"
+              />
+            }
+            label="Dining"
+          />{' '}
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={furniture}
+                onChange={handleChangeFurniture}
+                name="Living"
+              />
+            }
+            label="Living"
+          />{' '}
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={furniture}
+                onChange={handleChangeFurniture}
+                name="Office"
+              />
+            }
+            label="Office"
+          />{' '}
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={furniture}
+                onChange={handleChangeFurniture}
+                name="Kids & baby"
+              />
+            }
+            label="Kids & baby"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                checked={furniture}
+                onChange={handleChangeFurniture}
+                name="Organic Mattress"
+              />
+            }
+            label="Organic Mattress"
+          />
+        </FormGroup>
+      </FormControl>
+      <Typography varient="h6" gutterBottom>
+        Have you been in contact within the last 14 days with anyone who has
+        experienced any covid-symptoms?
+      </Typography>
+      <FormControl component="fieldset">
+        <RadioGroup
+          aria-label="Covid-19"
+          name="Covid19"
+          value={covid}
+          onChange={handleChangeCovid}
+        >
+          <FormControlLabel
+            value="Yes"
+            control={<Radio color="primary" />}
+            label="Yes"
+          />
+          <FormControlLabel
+            value="No"
+            control={<Radio color="primary" />}
+            label="No"
+          />
+        </RadioGroup>
+      </FormControl>
+    </Fragment>
+  );
+};
+
+export default BeforeVisit;
---
+++ client/src/components/layouts/BeforeVisit/style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+}));
+export default useStyles;
