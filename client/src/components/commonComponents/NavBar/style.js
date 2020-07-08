f733ebe0bb5fe457c99617cce2cbf6d06154ab3f
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-08 10:21:35 +0300

    modification on navbar to become closer to the original website navbar also make the book a visit tab active so it redirect the user to the reservation page #56

--- client/src/components/commonComponents/NavBar/style.js
+++ client/src/components/commonComponents/NavBar/style.js
@@ -4,50 +4,62 @@ const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: '#000000',
     color: 'white',
-    display: 'flex',
-    height: '40px',
-    justifyContent: 'center',
-    alignItems: 'center',
+    fontSize: '16px',
+    padding: '5px 15px',
+    textAlign: 'center',
   },
   logo: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '20px',
     fontFamily: 'Quicksand',
     fontSize: '25px',
   },
-  img: {
+  logoImg: {
     width: '60px',
     height: '60px',
+  },
+  blogImg: {
+    width: '220px',
   },
 
   list: {
     fontSize: '12px',
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+    [theme.breakpoints.up(600)]: {
       marginLeft: '65px',
+    },
+    [theme.breakpoints.down(970)]: {
+      display: 'none',
+    },
+  },
+  subheader: {
+    [theme.breakpoints.down(970)]: {
+      display: 'none',
+    },
+  },
+  icons: {
+    display: 'none',
+    [theme.breakpoints.down(970)]: {
+      display: 'flex',
+      margin: '10px 0',
     },
   },
   item: {
     color: '#90B27B',
     fontWeight: 'bold',
   },
-  intruduction: {
+  pagePath: {
     fontFamily: 'Quicksand',
     marginLeft: '20px',
     fontSize: '12px',
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+    [theme.breakpoints.up(600)]: {
       marginLeft: '80px',
       fontSize: '15px',
-      marginTop: '20px',
+      marginTop: '10px',
     },
   },
-  bookappointment: {
+  description: {
     fontSize: '18px',
     fontFamily: 'Quicksand',
     marginLeft: '20px',
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+    [theme.breakpoints.up(600)]: {
       fontSize: '28px',
       marginLeft: '80px',
     },
