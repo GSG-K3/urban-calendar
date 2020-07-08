8fd7cdd8e9e36ffb57dff262ef3c98824b7371e7
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-08 21:17:16 +0200

    change navbar styling and create a menu list on mobile view #56

--- client/src/components/commonComponents/NavBar/index.js
+++ client/src/components/commonComponents/NavBar/index.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { List, ListItem, Typography, Grid, Box, Link } from '@material-ui/core';
+import React, { useState, useRef } from 'react';
+import {
+  List,
+  ListItem,
+  Typography,
+  Box,
+  ClickAwayListener,
+  Grow,
+  Paper,
+  Popper,
+  MenuItem,
+  MenuList,
+  Divider,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import MenuIcon from '@material-ui/icons/Menu';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import logo from './logo.svg';
-import blog from './downearthblog.webp';
-
+import logo from '../../../assests/logo.svg';
+import blog from '../../../assests/downearthblog.webp';
+import { v4 as uuidv4 } from 'uuid';
 import useStyles from './style.js';
+
+const getTabs = () => [
+  'COLLECTIONS',
+  'FURNITURE',
+  'ORIGINAL MATTRESSES',
+  'LIGHTING & ACCENTS',
+  'GET TO KNOW US',
+  'BOOK A VISIT',
+];
 
 const NavBar = () => {
   const classes = useStyles();
+  const tabs = getTabs();
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <div>
-      <Grid container>
-        <Grid item xs={12} sm={12}>
-          <Typography className={classes.header}>
-            Our Showroom is Open! (By Appointment Only)
-          </Typography>
-        </Grid>
-      </Grid>
+      <Typography variant="h6" className={classes.header}>
+        Our Showroom is Open! (By Appointment Only)
+      </Typography>
+
       <Box
         display="flex"
         justifyContent="space-between"
@@ -30,69 +64,108 @@ const NavBar = () => {
         <Box display="flex" justifyContent="flex-end" m={1} p={1}>
           <Box p={1} display="flex">
             <SearchIcon />
-            <Typography>Search</Typography>
+            <Typography variant="subtitle1">Search</Typography>
           </Box>
           <Box p={1} display="flex">
             <ShoppingCartOutlinedIcon />
-            <Typography>0 items</Typography>
+            <Typography variant="subtitle1">0 items</Typography>
           </Box>
         </Box>
       </Box>
 
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        className={classes.logo}
-      >
-        <img src={logo} className={classes.logoImg} alt="logo" />
-        urban natural home
-      </Box>
+      <Link to={'/'} className={classes.homeLink}>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <img src={logo} className={classes.logoImg} alt="logo" />
+          <Typography color="secondary" variant="h4">
+            urban natural home
+          </Typography>
+        </Box>
+      </Link>
+
       <Box
         display="flex"
         justifyContent="space-around"
         className={classes.icons}
       >
-        <MenuIcon />
+        <MenuIcon
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        />
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom' ? 'center top' : 'center bottom',
+              }}
+            >
+              <Paper className={classes.menuList}>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow">
+                    {tabs.map((tab) =>
+                      tab === 'BOOK A VISIT' ? (
+                        <MenuItem
+                          onClick={handleClose}
+                          className={classes.bookTab}
+                          key={uuidv4()}
+                        >
+                          <Link to={'/guideline'} className={classes.bookTab}>
+                            {tab}
+                          </Link>
+                        </MenuItem>
+                      ) : (
+                        <MenuItem onClick={handleClose} key={uuidv4()}>
+                          {tab}
+                        </MenuItem>
+                      ),
+                    )}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+
         <ShoppingCartOutlinedIcon />
         <SearchIcon />
       </Box>
       <List className={classes.list}>
-        <Grid container>
-          <Grid item sm={3} md={2}>
-            <ListItem> COLLECTIONS</ListItem>
-          </Grid>
-          <Grid item sm={3} md={2}>
-            <ListItem> FURNITURE</ListItem>
-          </Grid>
-          <Grid item sm={3} md={2}>
-            <ListItem> ORIGINAL MATTRESSES</ListItem>
-          </Grid>
-          <Grid item sm={3} md={2}>
-            <ListItem> LIGHTING & ACCENTS </ListItem>
-          </Grid>
-          <Grid item sm={3} md={2}>
-            <ListItem> GET TO KNOW US</ListItem>
-          </Grid>
-          <Grid item sm={3} md={2}>
-            <ListItem className={classes.item}>
-              <Link href="/reservation">BOOK A VISIT</Link>{' '}
-            </ListItem>
-          </Grid>
-        </Grid>
+        <Box
+          display="flex"
+          justifyContent="center"
+          width={1}
+          alignItems="center"
+        >
+          {tabs.map((tab) =>
+            tab === 'BOOK A VISIT' ? (
+              <ListItem key={uuidv4()}>
+                <Link to={'/guideline'} className={classes.bookTab}>
+                  {tab}
+                </Link>
+              </ListItem>
+            ) : (
+              <ListItem key={uuidv4()}>{tab}</ListItem>
+            ),
+          )}
+        </Box>
       </List>
+      <Typography variant="caption" className={classes.pagePath}>
+        Home / Book an Appointment to Visit Our Showroom
+      </Typography>
+      <Typography variant="h4" className={classes.description}>
+        Book an Appointment to Visit Our Showroom
+      </Typography>
 
-      <div>
-        <Typography className={classes.pagePath}>
-          Home / Book an Appointment to Visit Our Showroom
-        </Typography>
-        <Typography className={classes.description}>
-          Book an Appointment to Visit Our Showroom
-        </Typography>
-      </div>
-      <Grid item xs={12}>
-        <hr className={classes.line} />
-      </Grid>
+      <Divider variant="middle" />
     </div>
   );
 };
--- client/src/components/commonComponents/NavBar/style.js
+++ client/src/components/commonComponents/NavBar/style.js
@@ -4,32 +4,28 @@ const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: '#000000',
     color: 'white',
-    fontSize: '16px',
     padding: '5px 15px',
     textAlign: 'center',
   },
-  logo: {
-    fontFamily: 'Quicksand',
-    fontSize: '25px',
+  subheader: {
+    [theme.breakpoints.down(970)]: {
+      display: 'none',
+    },
+  },
+  blogImg: {
+    width: '220px',
+  },
+  homeLink: {
+    textDecoration: 'none',
   },
   logoImg: {
     width: '60px',
     height: '60px',
   },
-  blogImg: {
-    width: '220px',
-  },
-
   list: {
-    fontSize: '12px',
     [theme.breakpoints.up(600)]: {
       marginLeft: '65px',
     },
-    [theme.breakpoints.down(970)]: {
-      display: 'none',
-    },
-  },
-  subheader: {
     [theme.breakpoints.down(970)]: {
       display: 'none',
     },
@@ -41,32 +37,29 @@ const useStyles = makeStyles((theme) => ({
       margin: '10px 0',
     },
   },
-  item: {
+  menuList: {
+    width: '100vw',
+  },
+  bookTab: {
     color: '#90B27B',
     fontWeight: 'bold',
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
   },
   pagePath: {
-    fontFamily: 'Quicksand',
     marginLeft: '20px',
-    fontSize: '12px',
     [theme.breakpoints.up(600)]: {
       marginLeft: '80px',
-      fontSize: '15px',
       marginTop: '10px',
     },
   },
   description: {
-    fontSize: '18px',
-    fontFamily: 'Quicksand',
     marginLeft: '20px',
     [theme.breakpoints.up(600)]: {
-      fontSize: '28px',
       marginLeft: '80px',
     },
-  },
-
-  line: {
-    width: '90vw',
   },
 }));
 
