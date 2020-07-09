a054996a8871178db38a8ea463d4cf8e4d74c740
tasneembhiri <141059@ppu.edu.ps>
2020-07-09 12:10:06 +0300

    add route for landing page #22

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Guideline from './components/layouts/Guideline';
 import Checkout from './components/commonComponents/TabsCheckout';
 import NavBar from './components/commonComponents/NavBar';
+import LandingPage from './components/layouts/LandingPage'
 
 const App = () => (
   <Fragment>
     <Router>
       <NavBar />
       <Switch>
+      <Route exact path="/" component={LandingPage} />
         <Route exact path="/guideline" component={Guideline} />
         <Route exact path="/reservation" component={Checkout} />
       </Switch>
