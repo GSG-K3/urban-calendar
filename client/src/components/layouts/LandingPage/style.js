1b1ddb7b96d7e95cc99c9cd6e4215c75c78142f4
tasneembhiri <141059@ppu.edu.ps>
2020-07-09 10:35:47 +0300

    delete negative number for margin and replace it positive using rem #22

--- client/src/components/layouts/LandingPage/style.js
+++ client/src/components/layouts/LandingPage/style.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   text: {
-    width: '70%',
+    width: '80%',
     fontFamily: 'Quicksand',
     fontSize: '15px',
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
@@ -16,9 +16,10 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     marginBottom: '15px',
     [theme.breakpoints.up(950 + theme.spacing(2) * 2)]: {
-      marginLeft: '-2rem',
-      marginBottom: 'none',
-      marginTop: '-2rem',
+      marginLeft: '1rem',
+      marginRight: '7rem',
+      marginBottom: '5rem',
+      marginTop: '1rem',
     },
   },
   link: {
@@ -32,7 +33,6 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up(950 + theme.spacing(2) * 2)]: {
       display: 'flex',
       flexDirection: 'row',
-      justifyContent: 'space-around',
     },
   },
   image: {
@@ -47,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
   bookappointment: {
     marginTop: '20px',
     [theme.breakpoints.up(950 + theme.spacing(2) * 2)]: {
-      marginLeft: '-1rem',
+      marginLeft: '2rem',
       marginRight: '20rem',
       marginTop: '30px',
     },
