7c49b75167a9470ac7f1d8f08bc79ebab960ecd0
tasneembhiri <141059@ppu.edu.ps>
2020-07-09 15:08:31 +0300

    modify customer_info table #62

--- server/database/dbBuild.sql
+++ server/database/dbBuild.sql
@@ -5,16 +5,16 @@ DROP TABLE if EXISTS customer_info CASCADE;
 
 CREATE TABLE customer_info(
   id SERIAL PRIMARY key NOT NULL,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
+  full_name VARCHAR(100) NOT NULL,
   phone NUMERIC NOT NULL,
   email VARCHAR(50) NOT NULL,
+  zip_code NUMERIC NOT NULL,
   reserve_date TIMESTAMP 
 
 );
 
 --example
-INSERT INTO customer_info(first_name, last_name,phone, email,reserve_date) VALUES ('mega','fly','05987648','mega90@gmail.com','2020-06-25 10:00:00') ;
+INSERT INTO customer_info(full_name, phone, email,zip_code,reserve_date) VALUES ('mega','05987648','mega90@gmail.com','12345','2020-06-25 10:00:00') ;
 
 
 COMMIT;
