965640e6009982e9e1d9fa9fc3b27e075531d4fa
Nujood <shawar.nujood@gmail.com>
2020-07-12 11:29:39 +0300

    spilling mistake has been corrected

--- client/src/components/layouts/ConfirmationTab/confirmationTab.jsx
+++ client/src/components/layouts/ConfirmationTab/confirmationTab.jsx
@@ -7,14 +7,14 @@ const Confirmation = () => (
       Thank you!
     </Typography>
     <Typography variant="h6" gutterBottom>
-      Our health and saftey rules
+      Our health and safety rules
     </Typography>
     <Typography variant="subtitle1">
       1. All visitors must wear face masks before entering the building.
       <br />
       2. No more than 3 People allowed to be in the showroom at once.
       <br />
-      3. Make sure to follow the American Red Cross Instuctions of Saftey.
+      3. Make sure to follow the American Red Cross Instuctions of Safety.
     </Typography>
   </Fragment>
 );
