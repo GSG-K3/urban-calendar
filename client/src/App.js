23b30a7179ef49694d9e2f7aa2577a46c41900e6
Nujood <shawar.nujood@gmail.com>
2020-07-12 19:32:53 +0300

    modifying the index.js of each component folder to consists of import-export lines only

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Guideline from './components/layouts/Guideline';
 import Checkout from './components/commonComponents/TabsCheckout';
 import NavBar from './components/commonComponents/NavBar';
-import LandingPage from './components/layouts/LandingPage'
+import LandingPage from './components/layouts/LandingPage';
 
 const App = () => (
   <Fragment>
