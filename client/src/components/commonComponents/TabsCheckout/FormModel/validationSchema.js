10165fe2670d09554f01278ea809079a26948236
Nujood <shawar.nujood@gmail.com>
2020-07-13 23:49:48 +0300

    adding the validation schema for before visit questions

--- client/src/components/commonComponents/TabsCheckout/FormModel/validationSchema.js
+++ client/src/components/commonComponents/TabsCheckout/FormModel/validationSchema.js
@@ -1,22 +1,41 @@
-import * as Yup from 'yup';
-import checkoutFormModel from './checkoutFormModel';
+import * as Yup from "yup";
+import checkoutFormModel from "./checkoutFormModel";
 
 const {
-  formField: { fullName, phone, email, zipCode },
+  formField: {
+    fullName,
+    phone,
+    email,
+    zipCode,
+    product,
+    covid19,
+    appointmentType,
+  },
 } = checkoutFormModel;
+
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 export default [
   Yup.object().shape({
     [fullName.name]: Yup.string().required(`${fullName.requiredErrorMsg}`),
+
     [phone.name]: Yup.string()
-      .matches(phoneRegExp, 'Phone number is not valid')
+      .matches(phoneRegExp, "Phone number is not valid")
       .required(`${phone.requiredErrorMsg}`),
     [email.name]: Yup.string()
-      .email('Invalid email')
+      .email("Invalid email")
       .required(`${email.requiredErrorMsg}`),
     [zipCode.name]: Yup.string()
-      .matches(/^[0-9]{5}$/, 'Must be exactly 5 digits')
+      .matches(/^[0-9]{5}$/, "Must be exactly 5 digits")
       .required(`${zipCode.requiredErrorMsg}`),
   }),
+  Yup.object().shape({
+    [appointmentType.name]: Yup.string().required(
+      "Fill at least one"
+    ),
+    [product.name]: Yup.array().required("At least one is required"),
+    [covid19.name]: Yup.string().required("This Field is required"),
+  }),
 ];
+
+// ).oneOf([true], 'Error'),
