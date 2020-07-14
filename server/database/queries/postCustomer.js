7eeada7fbf9f6d5160895f6fa6f6c40cb66ed1a1
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-14 14:40:57 +0300

    change the naming of reserved time

--- server/database/queries/postCustomer.js
+++ server/database/queries/postCustomer.js
@@ -1,11 +1,11 @@
 const dbConnection = require('../dbConnection');
 
 const postCustomer = (data) => {
-  const { fullName, phone, email, zipCode, reserveDate } = data;
+  const { fullName, phone, email, zipCode, appointmentTime } = data;
   const sql = {
     text:
       'INSERT INTO customer_info (full_name,phone,email,zip_code,reserve_date) VALUES ($1,$2,$3,$4,$5)',
-    values: [fullName, phone, email, zipCode, reserveDate],
+    values: [fullName, phone, email, zipCode, appointmentTime],
   };
   return dbConnection.query(sql.text, sql.values);
 };
