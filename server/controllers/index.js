0fc3c7fdf3679a6ed0629c0594745086e290fa69
tasneembhiri <141059@ppu.edu.ps>
2020-07-16 22:44:59 +0300

    add router for available time #74

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const customerInfo = require('./postCustomer');
+const timeAvailable = require('./availableTime');
 
 router.post('/api/questions/user-info', customerInfo);
+router.post('/api/availabletime', timeAvailable);
 module.exports = router;
