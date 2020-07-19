94483fd1e0ebcd4131ab06a720138b0b2be51b3a
tasneembhiri <141059@ppu.edu.ps>
2020-07-19 17:41:04 +0300

    add router for getRservationDate #77

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const customerInfo = require('./postCustomer');
 const availableTime = require('./availableTime');
+const getReservationData = require('./getReservationData');
 
 router.post('/api/questions/user-info', customerInfo);
 router.post('/api/availabletime', availableTime);
+router.post('/api/getReservationData', getReservationData);
 module.exports = router;
