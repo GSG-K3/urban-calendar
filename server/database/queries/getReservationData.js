7d75dc4d5c2c596229a013051647b902f7663fe3
tasneembhiri <141059@ppu.edu.ps>
2020-07-19 17:43:00 +0300

    create a query for getRservationDate #77

---
+++ server/database/queries/getReservationData.js
@@ -0,0 +1,13 @@
+const dbconnection = require('../dbConnection');
+
+const getResrvationData = (data) => {
+  const { startDate, endDate } = data;
+  const sql = {
+    text:
+      'select customer_info.reservation_date,customer_info.time_id,reservation_time.time_slot from customer_info join reservation_time on (customer_info.time_id=reservation_time.id) WHERE reservation_date >= $1 and reservation_date < $2',
+    values: [startDate, endDate],
+  };
+  return dbconnection.query(sql);
+};
+
+module.exports = getResrvationData;
