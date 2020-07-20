bb048f5cb793a73b0be390c93fc84d4d69e89ff0
tasneembhiri <141059@ppu.edu.ps>
2020-07-20 12:04:33 +0300

    add router.use for both error404 and error500  #79

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 const customerInfo = require('./postCustomer');
 const availableTime = require('./availableTime');
+const { Error404 } = require('../helpers/errors');
+const { Error500 } = require('../helpers/errors');
 
 router.post('/api/questions/user-info', customerInfo);
 router.post('/api/availabletime', availableTime);
+router.use(Error404);
+router.use(Error500);
 module.exports = router;
