d61042d4c63d5c1e821624ef56ef1613dfc40c66
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-20 19:43:51 +0300

    use axios all to call two requests (get all reservations data and reservation time slot) and handle their response to find the fully reserved days and the available time slots for the other days #28

--- client/src/components/layouts/Booking/Booking.jsx
+++ client/src/components/layouts/Booking/Booking.jsx
@@ -1,84 +1,166 @@
 import React, { useState, useEffect } from 'react';
 import InfiniteCalendar from 'react-infinite-calendar';
 import 'react-infinite-calendar/styles.css';
-import { Box } from '@material-ui/core';
+import { Box, CircularProgress, Typography } from '@material-ui/core';
 import moment from 'moment';
 import axios from 'axios';
-import RadioButton from '../BeforeVisit/Radio';
+import RadioButton from '../../commonComponents/TabsCheckout/FormFields/Radio';
+import exceptions from './exceptedDays';
 
-const covidBlockedDays = 14;
-const today = new Date();
-const blockedWeeks = new Date(
-  today.getFullYear(),
-  today.getMonth(),
-  today.getDate() + covidBlockedDays,
-);
-const publicHolidays = [
-  new Date(2020, 8, 7),
-  new Date(2020, 10, 11),
-  new Date(2020, 10, 26),
-  new Date(2020, 11, 25),
-];
+const { today, blockedWeeks, endDate, publicHolidays } = exceptions;
 const Booking = (props) => {
-  const [reservationDate, setReservationDate] = useState(null);
-  const [timeSlots, setTimeSlots] = useState([]);
-  let timeSlotsData = [];
+  const [allReservations, setAllReservations] = useState(null);
+  const [TimeSlots, setTimeSlots] = useState(null);
+  const [availableTimeSlots, setAvailableTimeSlots] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const fullReservedDays = [];
+  const storeTimeIds = {};
+  const storeAvailableTimeSlots = {};
 
   const {
     formField: { reservationTime },
     covidAnswer,
   } = props;
-  useEffect(() => {
-    if (reservationDate) {
-      axios
-        .post('/api/availabletime', { reservationDate })
-        .then((result) => setTimeSlots(result.data))
-        .catch((err) => err.response.data);
-    }
-  }, [reservationDate]);
 
-  if (timeSlots) {
-    timeSlotsData = timeSlots.map((time) => ({
+  // get reservations and time slots data when the app mount
+  useEffect(() => {
+    // request to get all Reservations Data among 3 months
+    const requestOne = axios.post('/api/reservationData', {
+      startDate: moment(today).format('YYYY-MM-DD'),
+      endDate: moment(endDate).format('YYYY-MM-DD'),
+    });
+    // request to get all Time Slots
+    const requestTwo = axios.get('/api/timeslots');
+
+    axios
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread((...responses) => {
+          setAllReservations(responses[0].data);
+          setTimeSlots(responses[1].data);
+          setIsLoading(false);
+        }),
+      )
+      .catch((err) => err.response.data);
+  }, []);
+
+  // store the fully reserved dates
+  if (allReservations && TimeSlots) {
+    // store each reservation date with it's reserved time id.
+    for (let i = 0; i < allReservations.length; i++) {
+      // change date format
+      const appointmentDate = moment(
+        allReservations[i].reservation_date,
+      ).format('YYYY-MM-DD');
+
+      // check if the date already stored then add all the reservations time ids for that day
+      if (storeTimeIds[appointmentDate]) {
+        storeTimeIds[appointmentDate].timeId = [
+          ...storeTimeIds[appointmentDate].timeId,
+          allReservations[i].time_id,
+        ];
+      }
+      // if the date doesn't exist in the store, store it with it's first reservation time id
+      else {
+        storeTimeIds[appointmentDate] = {
+          timeId: [allReservations[i].time_id],
+        };
+      }
+    }
+    // find the fully reserved date and the available time slots for the other days
+    const reservedDates = Object.keys(storeTimeIds); // get the reservation days from the store keys
+
+    // the fixed time slots ids that we get from DB
+    const timeSlotsIds = TimeSlots.map((timeSlot) => timeSlot.id);
+
+    for (let j = 0; j < reservedDates.length; j++) {
+      // check if one of the reservation dates is fully reserved to disable it later in the calender
+      if (
+        storeTimeIds[reservedDates[j]].timeId.length === timeSlotsIds.length
+      ) {
+        fullReservedDays.push(new Date(reservedDates[j]));
+      }
+      // compare reservation time ids with fixed time slots ids to get the available time slots ids
+      else {
+        const reservedIds = new Set(storeTimeIds[reservedDates[j]].timeId);
+        const difference = [...timeSlotsIds].filter(
+          (timeSlotId) => !reservedIds.has(timeSlotId),
+        );
+        storeAvailableTimeSlots[reservedDates[j]] = { difference };
+      }
+    }
+  }
+  // speacify the time slot labels and values when shown on radio buttons
+  const TimeSlotData = (timeSlot, reservationDate) =>
+    timeSlot.map((time) => ({
       value: time.id + reservationDate,
       label: time.time_slot,
     }));
-  }
-  const setDate = (date) =>
-    date && setReservationDate(moment(date).format('YYYY-MM-DD'));
+
+  // get the available time slots for a specific date
+  const getAvailableTimeSlots = (date) => {
+    const reservationDate = moment(date).format('YYYY-MM-DD');
+
+    if (storeAvailableTimeSlots[reservationDate]) {
+      const reservedIds = new Set(
+        storeAvailableTimeSlots[reservationDate].difference,
+      );
+      // compare the ids for the time slots of a specific date with the fixed timeSlot to return all the time slot data and send it to radio buttons.
+      const availableTime = TimeSlots.filter((timeSlot) =>
+        reservedIds.has(timeSlot.id),
+      );
+      setAvailableTimeSlots(TimeSlotData(availableTime, reservationDate));
+    } else {
+      setAvailableTimeSlots(TimeSlotData(TimeSlots, reservationDate));
+    }
+  };
+
   return (
-    <Box display="flex" flexDirection="column">
-      <InfiniteCalendar
-        width={400}
-        height={300}
-        shouldHeaderAnimate={true}
-        disabledDays={[0, 2]}
-        disabledDates={publicHolidays}
-        minDate={covidAnswer === 'yes' ? blockedWeeks : today}
-        selected={covidAnswer === 'yes' ? blockedWeeks : today}
-        onSelect={setDate}
-        theme={{
-          selectionColor: '#90B27A',
-          textColor: {
-            default: '#333',
-            active: '#FFF',
-          },
-          weekdayColor: '#90B27A',
-          headerColor: '#90B27A',
-          floatingNav: {
-            background: '#90B27A',
-            color: '#FFF',
-            chevron: '#FFA726',
-          },
-        }}
-      />
-      {reservationDate && (
-        <RadioButton
-          name={reservationTime.name}
-          label={reservationTime.label}
-          data={timeSlotsData}
-        />
+    <React.Fragment>
+      {isLoading ? (
+        <CircularProgress size={24} />
+      ) : (
+        <Box display="flex" flexDirection="column">
+          <Typography variant="h6">Please Select a date: </Typography>
+          <InfiniteCalendar
+            width={400}
+            height={300}
+            shouldHeaderAnimate={true}
+            disabledDays={[0, 2]}
+            disabledDates={[...publicHolidays, ...fullReservedDays]}
+            minDate={covidAnswer === 'yes' ? blockedWeeks : today}
+            min={today} // Minimum rendered month
+            max={endDate} // Maximum rendered month
+            selected={covidAnswer === 'yes' ? blockedWeeks : today}
+            onSelect={getAvailableTimeSlots}
+            theme={{
+              selectionColor: '#90B27A',
+              textColor: {
+                default: '#333',
+                active: '#FFF',
+              },
+              weekdayColor: '#90B27A',
+              headerColor: '#90B27A',
+              floatingNav: {
+                background: '#90B27A',
+                color: '#FFF',
+                chevron: '#FFA726',
+              },
+            }}
+          />
+          {availableTimeSlots && (
+            <Box>
+              <Typography variant="h6">Please Select a Time Slot: </Typography>
+              <RadioButton
+                name={reservationTime.name}
+                label={reservationTime.label}
+                data={availableTimeSlots}
+              />
+            </Box>
+          )}
+        </Box>
       )}
-    </Box>
+    </React.Fragment>
   );
 };
 export default Booking;
