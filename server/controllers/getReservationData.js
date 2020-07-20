1f6faca9df50919ab0c474a9409322edcdf1675c
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-20 21:08:07 +0300

    correct misspellins

--- server/controllers/getReservationData.js
+++ server/controllers/getReservationData.js
@@ -1,8 +1,8 @@
-const getResrvationData = require('../database/queries/getReservationData');
+const getReservationData = require('../database/queries/getReservationData');
 
 module.exports = (req, res) => {
   const data = req.body;
-  getResrvationData(data)
+  getReservationData(data)
     .then((result) => res.json(result.rows))
     .catch((err) => res.status(500).json({ message: err.message }));
 };
