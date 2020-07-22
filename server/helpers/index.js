1cd3cf170de6c7118fb148f1c01c417418f4e888
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-07-22 16:12:01 +0300

    add validation file into helpers folder

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,4 +1,5 @@
 const nodeMailer = require('./nodeMailer');
 const { Error404, Error500 } = require('./errors');
+const validation = require('./validation');
 
-module.exports = { nodeMailer, Error404, Error500 };
+module.exports = { nodeMailer, Error404, Error500, validation };
