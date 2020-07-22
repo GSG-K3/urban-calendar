e4d628f2adbc81910ca0fdfb227541d45959e1a7
Nujood <shawar.nujood@gmail.com>
2020-07-22 15:58:12 +0300

    adjusting the massage once again

--- server/helpers/nodeMailer.js
+++ server/helpers/nodeMailer.js
@@ -4,12 +4,11 @@ const { GMAIL_USER, GMAIL_PASS } = process.env;
 
 async function mailing(fullName, email, reservationDate, reservationTime) {
   const htmlEmail = `<h3>${fullName} thank you for reserving in our Store..</h3>
-  <p>Just Dieeeeeeeee, DIE pleassse, DO THE PLANET a favour and Die so the trees can grow better with your remains and we can make alot of furniture. JUSt DO NOT commit a SUICIDE, no one want to see your soul chasing them in their nightmares. <br/> 
   <ul>your reservation Date is: ${reservationDate}- ${reservationTime}EST</ul>
   <ul>Want to chat by phone to let us take your remains? Call us at (201) 330-1212</ul>
   <ul>Monday – Saturday, 10:00am – 6:00pm EST.</ul>
   <ul>Sundays, 12:00pm – 5:00pm EST </ul> 
-  <b> We wish you a great time with us, now and forever... RIP ${fullName}</b>`;
+  `;
 
   const transporter = nodemailer.createTransport({
     service: 'gmail',
