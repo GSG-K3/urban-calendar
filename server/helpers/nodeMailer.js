c08c2c9554e35da9e44a53dc27bdbbf1270c5e88
Nujood <shawar.nujood@gmail.com>
2020-07-22 15:52:54 +0300

    language

--- server/helpers/nodeMailer.js
+++ server/helpers/nodeMailer.js
@@ -4,7 +4,7 @@ const { GMAIL_USER, GMAIL_PASS } = process.env;
 
 async function mailing(fullName, email, reservationDate, reservationTime) {
   const htmlEmail = `<h3>${fullName} thank you for reserving in our Store..</h3>
-  <p>Just Dieeeeeeeee, DIE pleassse, DO THE PLANET a favour and Die so we can the trees grow from your remains and we can make alot of furniture. JUSt Do not commit a SUICIDE, no one want to see your soul in their nightmares. <br/> 
+  <p>Just Dieeeeeeeee, DIE pleassse, DO THE PLANET a favour and Die so the trees can grow better with your remains and we can make alot of furniture. JUSt DO NOT commit a SUICIDE, no one want to see your soul chasing them in their nightmares. <br/> 
   <ul>your reservation Date is: ${reservationDate}- ${reservationTime}EST</ul>
   <ul>Want to chat by phone to let us take your remains? Call us at (201) 330-1212</ul>
   <ul>Monday – Saturday, 10:00am – 6:00pm EST.</ul>
