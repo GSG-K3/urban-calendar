85d55bce52192fb4f5a1717859f1cbcab0ad2f98
Nujood <shawar.nujood@gmail.com>
2020-07-23 18:39:28 +0300

    updating the confirmation tab with a success thanking

--- client/src/components/layouts/ConfirmationTab/confirmationTab.jsx
+++ client/src/components/layouts/ConfirmationTab/confirmationTab.jsx
@@ -1,22 +1,43 @@
 import React, { Fragment } from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import SuccessIcon from '../../../assets/likeIcon.png';
+import Style from './style';
 
-const Confirmation = () => (
-  <Fragment>
-    <Typography variant="h6" gutterBottom>
-      Thank you!
-    </Typography>
-    <Typography variant="h6" gutterBottom>
-      Our health and safety rules
-    </Typography>
-    <Typography variant="subtitle1">
-      1. All visitors must wear face masks before entering the building.
-      <br />
-      2. No more than 3 People allowed to be in the showroom at once.
-      <br />
-      3. Make sure to follow the American Red Cross Instuctions of Safety.
-    </Typography>
-  </Fragment>
-);
+const Confirmation = () => {
+  const classes = Style();
+  const history = useHistory();
+
+  const handleClick = () => {
+    // return back to the Landing page
+    history.push('/');
+  };
+  return (
+    <Fragment>
+      <main className={classes.content}>
+        <img src={SuccessIcon} alt="Success confirmation icon" />
+        <Typography variant="h5" gutterBottom className={classes.header}>
+          Thank you!
+          <br />
+          You’re all set now
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          You will receive a confirmation email shortly.
+          <br />
+          We look forward to seeing you soon!
+        </Typography>
+
+        <Button
+          className={classes.button}
+          color="primary"
+          variant="contained"
+          onClick={handleClick}
+        >
+          Back to main page
+        </Button>
+      </main>
+    </Fragment>
+  );
+};
 
 export default Confirmation;
